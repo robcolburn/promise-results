6267e5e640299ca74d494dddb8c3ed44016bbbbe
Rob Colburn <rob@robcolburn.com>
2015-03-27 16:24:49 -0700

    Initial Drop

---
+++ test/test.js
@@ -0,0 +1,116 @@
+var results = require('../promise-results');
+
+describe('Assumptions', function() {
+  it('Chai validates valid promised array', function() {
+    return new Promise(function(r) {
+      setImmediate(function(){
+        r([2,3,4]);
+      });
+    }).should.eventually.eql([2,3,4]);
+  });
+  it('Chai validates wrong promised array', function() {
+    return new Promise(function(r) {
+      setImmediate(function(){
+        r([2,3,4]);
+      });
+    }).should.eventually.not.eql([2,3,5]);
+  });
+  it('Chai validates a valid promised object', function() {
+    return new Promise(function(r) {
+      setImmediate(function(){
+        r({a:2,b:3});
+      });
+    }).should.eventually.eql({a:2,b:3});
+  });
+  it('Chai validates a wrong promised object', function() {
+    return new Promise(function(r) {
+      setImmediate(function(){
+        r({a:2,b:3});
+      });
+    }).should.eventually.not.eql({a:2,b:4});
+  });
+  it('Chai validates rejections', function() {
+    return new Promise(function(s,e) {
+      setImmediate(function(){
+        e(new Error('nope'));
+      });
+    }).should.eventually.be.rejectedWith(Error);
+  });
+});
+
+describe('Promise Results', function() {
+  it('Resolves a valid array', function() {
+    return results([2,3,4]).should.eventually.eql([2,3,4]);
+  });
+  it('Resolves not accept a wrong array', function() {
+    return results([2,3,5]).should.eventually.not.eql([2,3,4]);
+  });
+  it('Resolves a valid object', function() {
+    return results({a:2,b:4}).should.eventually.eql({a:2,b:4});
+  });
+  it('Resolves a Promises array', function() {
+    return results([
+      Promise.resolve(2),
+      Promise.resolve(3),
+      Promise.resolve(4),
+    ]).should.eventually.eql([2,3,4]);
+  });
+  it('Resolves a Promises array with rejections', function() {
+    return results([
+      Promise.resolve(2),
+      Promise.reject(3),
+      Promise.reject(4),
+    ]).should.eventually.eql([2,3,4]);
+  });
+  it('Resolves a Promises object', function() {
+    return results({
+      a:Promise.resolve(2),
+      b:Promise.resolve(4),
+    }).should.eventually.eql({a:2,b:4});
+  });
+  it('Resolves a Promises object with rejections', function() {
+    return results({
+      a:Promise.resolve(2),
+      b:Promise.reject(4),
+    }).should.eventually.eql({a:2,b:4});
+  });
+  it('Resolve complex a Promises object', function() {
+    var a = Promise.resolve(2);
+    var b = a.then(function (r) {
+      return r + 1;
+    }).then(function (r) {
+      return r + 1;
+    });
+    return results({a:a,b:b}).should.eventually.eql({a:2,b:4});
+  });
+  it('Resolve mixed a Promise object', function() {
+    var a = Promise.resolve(2);
+    var b = a.then(function (r) {
+      return r + 1;
+    }).then(function (r) {
+      return r + 1;
+    });
+    var c = 4;
+    return results({a:a,b:b,c:c}).should.eventually.eql({a:2,b:4,c:4});
+  });
+  it('Resolve mixed a Promise object with rejections', function() {
+    var a = Promise.resolve(2);
+    var b = a.then(function (r) {
+      return r + 1;
+    }).then(function (r) {
+      return r + 1;
+    });
+    var c = Promise.reject(20);
+    return results({a:a,b:b,c:c}).should.eventually.eql({a:2,b:4,c:20});
+  });
+  it('Resolve, not acccept, mixed an invalid Promise object with rejections', function() {
+    var a = Promise.resolve(2);
+    var b = a.then(function (r) {
+      return r + 1;
+    }).then(function (r) {
+      return r + 1;
+    });
+    var c = Promise.reject(25);
+    return results({a:a,b:b,c:c}).should.eventually.not.eql({a:2,b:4,c:20});
+  });
+});
