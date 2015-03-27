ca0552fb78fb8046dc96609da1278f58597fdb55
Rob Colburn <rob@robcolburn.com>
2015-03-27 16:41:17 -0700

    Add allKeys feature, and break-up tests

---
+++ allKeys.js
@@ -0,0 +1,35 @@
+/**
+ * Promise to resolve once collection of promises has resolve or rejected.
+ *
+ * Loosely, based on promised-io/promise .allKeys, however:
+ *   - Expects global Promise (es6-promise).
+ *
+ * @param {object|array} collection
+ *   The collection of promises to wait for.
+ * @return Promise<object|array>
+ *   Resolved with a collection of results/rejections.
+ *   By convention, rejections should be instances of are Errors.
+ */
+module.exports = function promiseAllKeys (collection) {
+  return new Promise(function(resolve, reject) {
+    var array = Object.keys(collection);
+    var length = array.length;
+    var checkedOut = 0;
+    var results = Array.isArray(collection) ? [] : {};
+    if (length === 0) resolve(results);
+    else array.forEach(function checkIn (key) {
+      if (collection[key] instanceof Promise)
+        collection[key].then(checkOut, reject);
+      else
+        checkOut(collection[key]);
+
+      function checkOut(value) {
+        results[key] = value;
+        checkedOut++;
+        if (checkedOut === length) {
+          resolve(results);
+        }
+      }
+    });
+  });
+};
--- test/promise-results.js
+++ test/promise-results.js
@@ -1,43 +1,4 @@
 var results = require('../promise-results');
-
-describe('Assumptions', function() {
-  it('Chai validates valid promised array', function() {
-    return new Promise(function(r) {
-      setImmediate(function(){
-        r([2,3,4]);
-      });
-    }).should.eventually.eql([2,3,4]);
-  });
-  it('Chai validates wrong promised array', function() {
-    return new Promise(function(r) {
-      setImmediate(function(){
-        r([2,3,4]);
-      });
-    }).should.eventually.not.eql([2,3,5]);
-  });
-  it('Chai validates a valid promised object', function() {
-    return new Promise(function(r) {
-      setImmediate(function(){
-        r({a:2,b:3});
-      });
-    }).should.eventually.eql({a:2,b:3});
-  });
-  it('Chai validates a wrong promised object', function() {
-    return new Promise(function(r) {
-      setImmediate(function(){
-        r({a:2,b:3});
-      });
-    }).should.eventually.not.eql({a:2,b:4});
-  });
-  it('Chai validates rejections', function() {
-    return new Promise(function(s,e) {
-      setImmediate(function(){
-        e(new Error('nope'));
-      });
-    }).should.eventually.be.rejectedWith(Error);
-  });
-});
-
 describe('Promise Results', function() {
   it('Resolves a valid array', function() {
     return results([2,3,4]).should.eventually.eql([2,3,4]);
