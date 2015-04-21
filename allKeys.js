94d1f19fe2f6f499af333cf19ce759a20334e0ab
Rob Colburn <rob@robcolburn.com>
2015-04-21 16:12:28 -0700

    Add resultSet feature for partial results.
    * [x] Tests
    * [x] Update documentation
    * [x] Cleanup Array declaration

--- allKeys.js
+++ allKeys.js
@@ -7,15 +7,15 @@
  * @param {object|array} collection
  *   The collection of promises to wait for.
  * @return Promise<object|array>
- *   Resolved with a collection of results/rejections.
- *   By convention, rejections should be instances of are Errors.
+ *   Resolved with a collection of results.
+ *   Rejections with the rejection.
  */
 module.exports = function promiseAllKeys (collection) {
   return new Promise(function(resolve, reject) {
     var array = Object.keys(collection);
     var length = array.length;
     var checkedOut = 0;
-    var results = Array.isArray(collection) ? [] : {};
+    var results = Array.isArray(collection) ? new Array(length) : {};
     if (length === 0) resolve(results);
     else array.forEach(function checkIn (key) {
       if (collection[key] && typeof collection[key].then === 'function')
---
+++ test/resultSet.js
@@ -0,0 +1,106 @@
+var results = require('../resultSet');
+describe('Promise Result Sets', function() {
+  it('Resolves a valid array', function() {
+    return results([2,34]).should.eventually.eql([2,34]);
+  });
+  it('Resolves not accept a wrong array', function() {
+    return results([2,35]).should.eventually.not.eql([2,34]);
+  });
+  it('Resolves a valid object', function() {
+    return results({a:2,b:4}).should.eventually.eql({a:2,b:4});
+  });
+  it('Resolves a Promises array', function() {
+    return results([
+      Promise.resolve(2),
+      Promise.resolve(34),
+    ]).should.eventually.eql([2,34]);
+  });
+  it('Resolves for Promises/A compliant promises', function() {
+    return results([
+      Promise.resolve(2),
+      {then: function (s) {
+        s(34);
+      }},
+    ]).should.eventually.eql([2,34]);
+  });
+  it('Rejects a Promises array with rejections', function() {
+    return results([
+      Promise.resolve(2),
+      Promise.reject(34),
+    ]).should.be.rejectedWith({
+      resolved: [2, undefined],
+      rejected: [undefined, 34]
+    });
+  });
+  it('Rejects for Promises/A compliant promises', function() {
+    return results([
+      Promise.resolve(2),
+      {then: function (s, e) {
+        e(34);
+      }},
+    ]).should.be.rejectedWith({
+      resolved: [2, undefined],
+      rejected: [undefined, 34]
+    });
+  });
+  it('Resolves a Promises object', function() {
+    return results({
+      a:Promise.resolve(2),
+      b:Promise.resolve(4),
+    }).should.eventually.eql({a:2,b:4});
+  });
+  it('Resolves a Promises object with rejections', function() {
+    return results({
+      a:Promise.resolve(2),
+      b:Promise.reject(4),
+    }).should.be.rejectedWith({
+      resolved: {a: 2},
+      rejected: {b: 4}
+    });
+  });
+  it('Resolve complex a Promises object', function() {
+    var a = Promise.resolve(2);
+    var b = a.then(function (r) {
+      return r + 1;
+    }).then(function (r) {
+      return r + 1;
+    });
+    return results({a:a,b:b}).should.eventually.eql({a:2, b:4});
+  });
+  it('Resolve mixed a Promise object', function() {
+    var a = Promise.resolve(2);
+    var b = a.then(function (r) {
+      return r + 1;
+    }).then(function (r) {
+      return r + 1;
+    });
+    var c = 4;
+    return results({a:a,b:b,c:c}).should.eventually.eql({a:2,b:4,c:4});
+  });
+  it('Rejects mixed a Promise object with rejections', function() {
+    var a = Promise.resolve(2);
+    var b = a.then(function (r) {
+      return r + 1;
+    }).then(function (r) {
+      return r + 1;
+    });
+    var c = Promise.reject(20);
+    return results({a:a,b:b,c:c}).should.be.be.rejectedWith({
+      resolved: {a: 2, b: 4},
+      rejected: {c: 20}
+    });
+  });
+  it('Resolve, not acccept, mixed an invalid Promise object with rejections', function() {
+    var a = Promise.resolve(2);
+    var b = a.then(function (r) {
+      return r + 1;
+    }).then(function (r) {
+      return r + 1;
+    });
+    var c = Promise.reject(25);
+    return results({a:a,b:b,c:c}).should.not.be.rejectedWith({
+      resolved: {a: 2, b: 4},
+      rejected: {c: 25}
+    });
+  });
+});
